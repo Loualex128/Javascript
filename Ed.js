d028b9389123cb20085dd20215c3d1082cfacc47
Darkista <loualex128@gmail.com>
2022-04-27 22:11:08 -0400

    Primera clase del proyecto geometrico

---
+++ Ed.js
@@ -0,0 +1,14 @@
+var promedio = 0;
+var num1 = 35;
+var num2 = 32;
+var num3 = 177
+
+function Promedio (num1,num2,num3){
+
+    promedio = (num1 + num2 + num3) / 3;
+
+}
+
+Promedio(num1,num2,num3);
+
+console.log("El promedio es: " + promedio);
