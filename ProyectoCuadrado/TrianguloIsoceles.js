2955fcbb4eab6ebc276ad208847a38a0e9ab992d
Darkista <loualex128@gmail.com>
2022-05-16 11:53:46 -0400

    Inicio del proyecto de la tienda

--- ProyectoCuadrado/TrianguloIsoceles.js
+++ ProyectoCuadrado/TrianguloIsoceles.js
@@ -1,12 +1,14 @@
 var lados = 4;
-var base = 5;
+var base = 4;
 var altura;
 
-function calcularAltura(){
+function calcularAltura(lados,base){
     if (lados != base){
-        altura = Math.sqrt((lados * lados)-(base * base/4));
+        altura = Math.sqrt(lados**2-(base**2/4));
         console.log("La altura del triangulo isoceles es: " + altura);
     } else {
         console.log("Esto no es un triangulo isoceles");
     }
 }
+
+calcularAltura(lados,base);
